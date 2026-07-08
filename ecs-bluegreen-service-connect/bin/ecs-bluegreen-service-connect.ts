654065d72464cdb2317f6512675670146726509c
Olly Pomeroy <opomer@amazon.co.uk>
2026-07-08 12:25:24 +0000

    Bump node deps
    Signed-off-by: Olly Pomeroy <opomer@amazon.co.uk>

--- ecs-bluegreen-service-connect/bin/ecs-bluegreen-service-connect.ts
+++ ecs-bluegreen-service-connect/bin/ecs-bluegreen-service-connect.ts
@@ -5,7 +5,7 @@ import { EcsBlueGreenServiceConnectStack } from '../lib/ecs-bluegreen-service-co
 const app = new cdk.App();
 
 const yourIpAddress = app.node.tryGetContext('yourIpAddress') || '0.0.0.0/0';
-const region = app.node.tryGetContext('region') || process.env.CDK_DEFAULT_REGION;
+const region = app.node.tryGetContext('region') || process.env.CDK_DEFAULT_REGION || 'eu-west-1';
 
 new EcsBlueGreenServiceConnectStack(app, 'EcsBlueGreenServiceConnectStack', {
     env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: region },
